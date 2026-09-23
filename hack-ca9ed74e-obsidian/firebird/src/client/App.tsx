634ef39fc2a678ce5e06acf79d6acbba2b19d8b3
Kirpi4k <am12012008@gmail.com>
2026-09-23 16:33:39 +0500

    MVP: polish empty state layout

--- hack-ca9ed74e-obsidian/firebird/src/client/App.tsx
+++ hack-ca9ed74e-obsidian/firebird/src/client/App.tsx
@@ -387,7 +387,7 @@ export function App() {
 
             {!result && !submitError && (
               <div className="empty-state">
-                <Sparkles size={34} />
+                <Sparkles size={44} />
                 <p>Выберите параметры или один из демо-запросов. Здесь появится до трех карточек с проверяемыми причинами выбора.</p>
               </div>
             )}
